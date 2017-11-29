bc6c6abb53e565b063fa61437df25867458064b9
Yuanchen Zhang <alfmunny@gmail.com>
2017-11-29 18:23:15 +0100

    add the virtual machine

--- c.c
+++ c.c
@@ -133,4 +133,22 @@ int main(int argc, char **argv) {
     }
     
     memset(text, 0, poolsize);
-
+    memset(data, 0, poolsize);
+    memset(stack, 0, poolsize);
+    
+    bp = sp = (int *)((int)stack + poolsize);
+    ax = 0;
+    i = 0;
+    text[i++] = IMM;
+   	text[i++] = 10;
+    text[i++] = PUSH;
+    text[i++] = IMM;
+    text[i++] = 20;
+    text[i++] = ADD;
+    text[i++] = PUSH;
+    text[i++] = EXIT;
+ 
+   	pc = text;
+    program();
+    return eval();
+}
